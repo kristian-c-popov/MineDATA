a5a89685e48d9a73049c39847f681bb7d14a0481
kristian-c-popov <97772613+kristian-c-popov@users.noreply.github.com>
2025-07-10 15:50:22 -0400

    Update config.py

--- mineclip/config.py
+++ mineclip/config.py
@@ -1,4 +1,15 @@
-def get_config(variant="attn", ckpt_path=None):
+# def get_config(variant="attn", ckpt_path=None):
+#     return {
+#         "arch": "vit_base_p16_fz.v2.t2",
+#         "hidden_dim": 512,
+#         "image_feature_dim": 512,
+#         "mlp_adapter_spec": "v0-2.t0",
+#         "pool_type": "attn" if variant == "attn" else "avg",
+#         "resolution": [160, 256],
+#         "ckpt_path": ckpt_path
+#     }
+
+def get_config(variant="attn", ckpt_path=None, checksum=None):
     return {
         "arch": "vit_base_p16_fz.v2.t2",
         "hidden_dim": 512,
@@ -6,5 +17,11 @@ def get_config(variant="attn", ckpt_path=None):
         "mlp_adapter_spec": "v0-2.t0",
         "pool_type": "attn" if variant == "attn" else "avg",
         "resolution": [160, 256],
-        "ckpt_path": ckpt_path
+        "video_length": 60,
+
+        # ✅ Add this block to support model.load_ckpt()
+        "ckpt": {
+            "path": ckpt_path,
+            "checksum": checksum,
+        }
     }
